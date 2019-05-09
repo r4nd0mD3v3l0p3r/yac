a5a84552466fd48dcba036fd46a978a673eb4c1f
r4nd0md3v3l0p3r <lorenzo.chiovini@hotmail.it>
2019-05-09 14:43:50 +0200

    db setup

--- backend/dbManager.js
+++ backend/dbManager.js
@@ -1,11 +1,12 @@
 import Datastore from 'nedb'
 
 var usersDb = new Datastore()
+var chatRooms = new Datastore()
 
 export function setupDb() {
     console.log('setting up db')
     setupUsers()
-
+    setupRooms()
     console.log('db setup done')
 }
 
@@ -15,4 +16,11 @@ const setupUsers = () => {
                     { name: 'Penny', password: 'Penny', online: false},
                     { name: 'Sheldon Cooper', password: 'Sheldon', online: false}
                    ])
+}
+
+const setupRooms = () => {
+    chatRooms.insert([{name: 'Sports', loggedUsers: [], messages: []}, 
+                      {name: 'Music', loggedUsers: [], messages: []},
+                      {name: 'Weather', loggedUsers: [], messages: []}])
+
 }
